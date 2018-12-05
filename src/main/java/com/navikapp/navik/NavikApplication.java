a153b7af15e7a35573905b0013d74c3a9abf1095
domoholic <lukasx05@gmail.com>
2018-12-05 23:43:00 +0100

    Initial commit

---
+++ src/main/java/com/navikapp/navik/NavikApplication.java
@@ -0,0 +1,12 @@
+package com.navikapp.navik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavikApplication.class, args);
+	}
+}

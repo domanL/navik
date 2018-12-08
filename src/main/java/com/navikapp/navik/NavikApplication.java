af0f993bee497c3832f6d46673cd09e857494ed4
domanL <l.domanski20@gmail.com>
2018-12-08 11:40:35 +0100

    Solved running issue and fix connection to MS SQL

--- src/main/java/com/navikapp/navik/NavikApplication.java
+++ src/main/java/com/navikapp/navik/NavikApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NavikApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(NavikApplication.class, args);
 	}
